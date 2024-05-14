334ac942fcd2fdeb4c8ab42fdf7bf2af82774945
Fermenade <93608551+Fermenade@users.noreply.github.com>
2024-05-14 09:02:20 +0100

    Minergame 0.5 fix
    Added pictures
    performance fix
    Added a debugger(known issue: broken)

--- Script/Miner.js
+++ Script/Miner.js
@@ -1,17 +1,25 @@
 const blockSize = 16*5;
 let arrRollBack = Array.from(document.getElementsByClassName("rollingBack"));
-let arrCurRollBack = arrRollBack[arrRollBack.length-1];
+let CurRollBack = arrRollBack[arrRollBack.length-1];
+const boundary = blockSize*2;
+const checkUpLine = 3;
+let emptySize = 0;
 function init() {
-    genBackStartRow();
-    genBackRow();
-    genBackRow();
+    debug();
+    document.getElementById("container").style = `margin-top: -${boundary*2}px;`
+    for (let i=0; i<15;i++)genDwnBackRow();
     styleDiv();
 }
 function baseHandler(){
-    if (screenY-100<=arrRollBack[arrRollBack.length-2].screenY){
-      //  return;
-    }
-    genBackRow()
+    debugger;
+    //if (document.getElementById("container").getBoundingClientRect().top>=0);
+    if (arrRollBack[checkUpLine].getBoundingClientRect().bottom<window.screen.height-boundary){rmUpBackRow();genDwnBackRow();addEmpty();}
+    else if (arrRollBack[checkLowLine()].getBoundingClientRect().top>boundary){rmDwnBackRow();genUpBackRow();rmEmpty();}
+    else genDwnBackRow();
+    // console.log(window.screen.height-boundary);
+    // console.log(arrRollBack[checkLowLine()].getBoundingClientRect().bottom);
+    // console.log(arrRollBack[checkUpLine].getBoundingClientRect().top);
+    debugStyleDynamic();
 }
 function styleDiv(){
         //arrCurRollBack.style.height = `${blockSize}px`;
@@ -19,16 +27,10 @@ function styleDiv(){
         const rules = document.styleSheets[0].cssRules;
         rules[0].style = `height: ${blockSize}px`;
         rules[0].style = `background-size: ${blockSize}px;height: ${blockSize}px`;
+        debugStyleDynamic();
 }
 
-function genBackStartRow(){
-    var newDiv = document.createElement("div");
-    const parent = document.getElementById("container");
-
-    newDiv.setAttribute("class","rollingStartBack")
-    parent.appendChild(newDiv)
-}
-function genBackRow(){
+function genDwnBackRow(){
     var newDiv = document.createElement("div");
     const parent = document.getElementById("container");
     //insert new div
@@ -36,15 +38,74 @@ function genBackRow(){
     parent.appendChild(newDiv);
 
     //Updates the ElementList
+    // function?
     arrRollBack = Array.from(document.getElementsByClassName("rollingBack"));
-    arrCurRollBack = arrRollBack[arrRollBack.length-1];
+    CurRollBack = arrRollBack[arrRollBack.length-1];
 
     genIMGBackRow();
+}
+function genUpBackRow(){
+    var newDiv = document.createElement("div");
+    const parent = document.getElementById("container");
+    //insert new div
+    newDiv.setAttribute("class","rollingBack")
+    parent.insertBefore(newDiv,arrRollBack[0]);
+
+    //Updates the ElementList
+    arrRollBack = Array.from(document.getElementsByClassName("rollingBack"));
+    CurRollBack = arrRollBack[arrRollBack.length-1];
+    // link zu genimgback fehlt
+    arrRollBack[0].style.backgroundImage = 'url("../Assets/deepslate.png")'
 }
 function genIMGBackRow(){
         // IMG background
         // work in progress
-        arrCurRollBack.style.backgroundImage = 'url("../../../../Downloads/Projekt/Media/cobblestone1.png")';
+        CurRollBack.style.backgroundImage = 'url("../Assets/cobblestone.png")';
+}
+function rmDwnBackRow(){
+    CurRollBack.remove();
+    CurRollBack = arrRollBack[arrRollBack.length-1];
+}
+function rmUpBackRow(){
+    arrRollBack[0].remove();
+}
+//not final function name
+function addEmpty(){
+    emptySize += blockSize;
+    document.getElementById("emptiness").style.height = `${emptySize}px`;
+}
+function rmEmpty(){
+    emptySize -= blockSize;
+    document.getElementById("emptiness").style.height = `${emptySize}px`;
+}
+function checkLowLine(){
+    return arrRollBack.length-1-checkUpLine;
+}
+let arrRollDebug = Array.from(document.getElementsByClassName("debugElem"));
+function debug(){
+    for (let i = 0;i<4;i++)debugDraw();
+    //Updates the ElementList
+    arrRollDebug = Array.from(document.getElementsByClassName("debugElem"));
+    debugStyleStatic();
+}
+function debugDraw(){
+    var newDiv = document.createElement("div");
+    const parent = document.getElementById("debugItems");
+    //insert new div
+    newDiv.setAttribute("class","debugElem");
+    parent.insertBefore(newDiv,document.getElementById("container"));
+}
+function debugStyleStatic(){
+    // first and third elems are the visual boundaries
+    // others are the current elements, which get checked
+    arrRollDebug[0].style = `height: ${boundary}px; top:0px; background-color: rgba(255, 0, 0, 0.5); position: -webkit-sticky; position: sticky;`;//smart or stupid?
+    arrRollDebug[1].style = `height: ${boundary}px; background-color: rgba(255, 0, 0, 0.5); position: -webkit-sticky; position: sticky; top: ${screen.height}px;`; // tried to solve it with a for loop but it wouldnt work, so there you go
+}
+function debugStyleDynamic(){
+    arrRollDebug[2].style = `bottom: ${-arrRollBack[checkUpLine].getBoundingClientRect().bottom}px; background-color: rgba(0,0,255,0.5); position: fixed; height: 10px;`;
+    arrRollDebug[3].style = `bottom: ${arrRollBack[checkLowLine()].getBoundingClientRect().bottom}px; background-color: rgba(0,0,255,0.5); position: fixed; height: 10px;`;
+    console.log(arrRollBack[checkUpLine].getBoundingClientRect().bottom);
+    console.log(arrRollBack[checkLowLine()].getBoundingClientRect().bottom);
 }
 document.addEventListener("DOMContentLoaded",init);
 document.addEventListener("scroll",baseHandler)
